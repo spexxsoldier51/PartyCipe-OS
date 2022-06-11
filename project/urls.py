aa9e111ff1be2758be32ba5b67a2321e7a30bc57
spexxsoldier51 <75441752+spexxsoldier51@users.noreply.github.com>
2022-06-11 17:46:16 +0200

    Add files via upload

--- project/urls.py
+++ project/urls.py
@@ -1,21 +1,5 @@
-from django.conf import settings
-from django.conf.urls import include, url
-from django.contrib import admin
-
-from welcome.views import index, health
+from django.urls import include, path
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'project.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^$', index),
-    url(r'^health$', health),
-    url(r'^admin/', include(admin.site.urls)),
+    path('', include('partycipe.urls')),
 ]
-
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns = [
-        url(r'^__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
