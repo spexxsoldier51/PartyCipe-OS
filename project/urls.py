94cdfd546be5bd4acba87abdd10006eadb755cf7
spexxsoldier51 <75441752+spexxsoldier51@users.noreply.github.com>
2022-06-13 15:44:34 +0200

    Update urls.py

--- project/urls.py
+++ project/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import include, path
+from django.conf import settings
 
 urlpatterns = [
     path('', include('partycipe.urls')),
