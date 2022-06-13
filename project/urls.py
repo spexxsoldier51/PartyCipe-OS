b8fe3f761ec2bc97425672579711957e52fd4b68
spexxsoldier51 <75441752+spexxsoldier51@users.noreply.github.com>
2022-06-13 16:37:23 +0200

    Update urls.py

--- project/urls.py
+++ project/urls.py
@@ -22,29 +22,27 @@ from partycipe.views import *
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
-    path('', index, name='home'),
-    path('health/', health),
     path('admin/', admin.site.urls),
 
 
     path('accounts/', include('django.contrib.auth.urls')),
     path('signup/', SignUpView.as_view(), name='signup'),
 
-    path('partycipe/', Home),
+    path('', Home),
 
-    path('partycipe/party/join', JoinParty),
-    path('partycipe/party/partipate', JoinParty),
-    path('partycipe/party/create', create_party),
-    path('partycipe/party/<int:id>', party_detail, name='party_detail'),
+    path('party/join', JoinParty),
+    path('party/partipate', JoinParty),
+    path('party/create', create_party),
+    path('party/<int:id>', party_detail, name='party_detail'),
 
 
 
-    path('partycipe/participate/change/<int:id>', ChangeParticipate, name="ChangeParticipate"),
+    path('participate/change/<int:id>', ChangeParticipate, name="ChangeParticipate"),
 
-    re_path(r'partycipe/party\/(?P<signed_pk>(?:[0-9]+\/[A-Za-z0-9_=-]+))$', party_status, name='party-status'),
-    re_path(r'partycipe/party\/(?P<signed_pk>(?:[0-9]+\/[A-Za-z0-9_=-]+))\/join$', party_join, name='party-join'),
+    re_path(r'party\/(?P<signed_pk>(?:[0-9]+\/[A-Za-z0-9_=-]+))$', party_status, name='party-status'),
+    re_path(r'party\/(?P<signed_pk>(?:[0-9]+\/[A-Za-z0-9_=-]+))\/join$', party_join, name='party-join'),
 
-    re_path(r'partycipe/party\/(?P<signed_pk>(?:[0-9]+\/[A-Za-z0-9_=-]+))\/mail$', send_mail, name='send_mail'),
+    re_path(r'party\/(?P<signed_pk>(?:[0-9]+\/[A-Za-z0-9_=-]+))\/mail$', send_mail, name='send_mail'),
 ]
 
 if settings.DEBUG:
